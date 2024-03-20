212ccdb933a4c1bbe02d08bee04ea3b4e349b65e
kvd <vodangkhoangute@gmail.com>
2024-03-21 00:38:13 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+// const bodyParser = require("body-parser");
 
 var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse",upload.single('upfile'),(req, res) => {
+//  console.log(req.file);
+console.log(req.file)
+ res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size}) 
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
