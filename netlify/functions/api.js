7f49c1d925b5a9c0d1ce786b52d5ca2387b7126e
khoang vo <vodangkhoangute@gmail.com>
2024-04-08 10:30:52 +0000

    update for CD

---
+++ netlify/functions/api.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const serverless = require('serverless-http');
+// const bodyParser = require("body-parser");
+
+var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse",upload.single('upfile'),(req, res) => {
+//  console.log(req.file);
+console.log(req.file)
+ res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size}) 
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
+
+
+module.exports.handler = serverless(app);
